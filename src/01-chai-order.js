439f3458e43da0bcea9adf54b62294f15b689ad6
Utkarsh Kumawat <anshukumawat77955@gmail.com>
2026-03-05 08:40:29 +0530

    001 done

--- src/01-chai-order.js
+++ src/01-chai-order.js
@@ -47,20 +47,42 @@
  */
 export function getChaiOrderLength(order) {
   // Your code here
+  if ( typeof order != "string") return -1;
+  order = order.trim();
+  return order.length;
 }
 
 export function shoutChaiOrder(order) {
   // Your code here
+  if ( typeof order != "string") return "";
+  order = order.trim();
+  if (order.length == 0) return "";
+  return order.toUpperCase();
 }
 
 export function whisperChaiOrder(order) {
   // Your code here
+    if ( typeof order != "string") return "";
+  order = order.trim();
+  if (order.length == 0) return "";
+  return order.toLowerCase();
 }
 
 export function hasSpecialIngredient(order, ingredient) {
   // Your code here
+  if ( typeof order != "string") return false;
+  order = order.trim();
+  if (order.length == 0) return false;
+  order = order.toLowerCase();
+   if (typeof ingredient !== 'string') return false;
+  ingredient = ingredient.toLowerCase();
+  return order.includes(ingredient);
 }
 
 export function getFirstAndLastChar(order) {
   // Your code here
+    if ( typeof order != "string") return null;
+  order = order.trim();
+  if (order.length == 0) return null;
+  return { first: order.charAt(0), last: order.at(-1) }
 }
